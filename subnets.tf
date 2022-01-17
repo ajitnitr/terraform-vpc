3adb7c5093a0923c687b9bda8af042e2497487ec
Sriharsha Kalluru <sriharsha.kalluru@cantire.com>
2022-01-17 06:47:00 +0530

    Update terraform-vpc/

--- subnets.tf
+++ subnets.tf
@@ -1,7 +1,8 @@
 resource "aws_subnet" "public-subnets" {
-  count      = length(var.PUBLIC_SUBNET_CIDR)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.PUBLIC_SUBNET_CIDR[count.index]
+  count             = length(var.PUBLIC_SUBNET_CIDR)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.PUBLIC_SUBNET_CIDR[count.index]
+  availability_zone = var.AZS[count.index]
 
   tags = {
     Name = "${var.ENV}-public-subnet-${count.index + 1}"
@@ -10,9 +11,10 @@ resource "aws_subnet" "public-subnets" {
 }
 
 resource "aws_subnet" "private-subnets" {
-  count      = length(var.PRIVATE_SUBNET_CIDR)
-  vpc_id     = aws_vpc.main.id
-  cidr_block = var.PRIVATE_SUBNET_CIDR[count.index]
+  count             = length(var.PRIVATE_SUBNET_CIDR)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.PRIVATE_SUBNET_CIDR[count.index]
+  availability_zone = var.AZS[count.index]
 
   tags = {
     Name = "${var.ENV}-private-subnet-${count.index + 1}"
